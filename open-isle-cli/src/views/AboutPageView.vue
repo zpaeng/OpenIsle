0caed0187ff281abb3e82ec081ef4744fd85d0cb
Tim <135014430+nagisa77@users.noreply.github.com>
2025-07-03 14:35:46 +0800

    feat(cli): add message and about views

---
+++ open-isle-cli/src/views/AboutPageView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about-page">
+    关于页面内容
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPageView'
+}
+</script>
+
+<style scoped>
+.about-page {
+  padding: 20px;
+}
+</style>
---
+++ open-isle-cli/src/views/MessagePageView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="message-page">
+    消息页内容
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MessagePageView'
+}
+</script>
+
+<style scoped>
+.message-page {
+  padding: 20px;
+}
+</style>
