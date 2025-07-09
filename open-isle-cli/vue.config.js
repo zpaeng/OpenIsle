894c0e3a72ac76676e7e8b9929bf24c158727d44
tim <nagisa12164@163.com>
2025-07-09 16:17:56 +0800

    feat: update

--- open-isle-cli/vue.config.js
+++ open-isle-cli/vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+    publicPath: process.env.NODE_ENV === 'production'
+    // ? '/ai-cv/' 
+    ? '/OpenIsle/' 
+    : '/',
   transpileDependencies: true,
   configureWebpack: {
     devtool: 'source-map'
