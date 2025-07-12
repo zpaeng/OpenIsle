1c153cc18c8dc089a369b80b863b7ec8cdb45fdf
tim <nagisa12164@163.com>
2025-07-12 11:29:12 +0800

    feat: add cros

--- open-isle-cli/functions/api.js
+++ open-isle-cli/functions/api.js
@@ -1,9 +1,21 @@
+// open-isle-cli/functions/api/[...path].js
 export async function onRequest({ request }) {
   const url = new URL(request.url)
-  url.hostname = '129.204.254.110'
-  url.port     = '8080'
-  url.protocol = 'http:'
 
-  // 直接透传：不区分 user/category
-  return fetch(url, request)
+  // 把路径原封不动转发到广州裸 IP
+  url.hostname = "129.204.254.110"
+  url.port     = "8080"
+  url.protocol = "http:"
+
+  const resp = await fetch(url, request)
+
+  // 附加 CORS 头
+  return new Response(resp.body, {
+    status:  resp.status,
+    headers: {
+      ...resp.headers,
+      "Access-Control-Allow-Origin": "https://9ac7b637-openisle.cjt807916.workers.dev",
+      "Access-Control-Allow-Credentials": "true"
+    }
+  })
 }
