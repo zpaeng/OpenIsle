577edc8caca46def7fccce6e0e67e8b45a5f56a6
tim <nagisa12164@163.com>
2025-07-13 09:30:17 +0800

    feat: test code

--- open-isle-cli/src/main.js
+++ open-isle-cli/src/main.js
@@ -9,11 +9,11 @@ import { checkToken, clearToken } from './utils/auth'
 import { initTheme } from './utils/theme'
 
 // Configurable API domain and port
-// export const API_DOMAIN = 'http://127.0.0.1'
-// export const API_PORT = 8081
+export const API_DOMAIN = 'http://127.0.0.1'
+export const API_PORT = 8081
 
-export const API_DOMAIN = 'http://47.82.99.208'
-export const API_PORT = 8080
+// export const API_DOMAIN = 'http://47.82.99.208'
+// export const API_PORT = 8080
 
 // export const API_BASE_URL = API_PORT ? `${API_DOMAIN}:${API_PORT}` : API_DOMAIN
 export const API_BASE_URL = "";
