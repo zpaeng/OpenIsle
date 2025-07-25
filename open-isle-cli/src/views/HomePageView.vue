3c9c502da98ba1d037e54ac4075555397096c6b1
Tim <nagisa12164@163.com>
2025-07-25 14:12:57 +0800

    feat: delete code

--- open-isle-cli/src/views/HomePageView.vue
+++ open-isle-cli/src/views/HomePageView.vue
@@ -111,7 +111,7 @@ import { ref, onMounted, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { stripMarkdown } from '../utils/markdown'
 import { API_BASE_URL } from '../main'
-import { getToken, isLogin } from '../utils/auth'
+import { getToken } from '../utils/auth'
 import TimeManager from '../utils/time'
 import CategorySelect from '../components/CategorySelect.vue'
 import TagSelect from '../components/TagSelect.vue'
@@ -120,7 +120,6 @@ import ArticleCategory from '../components/ArticleCategory.vue'
 import SearchDropdown from '../components/SearchDropdown.vue'
 import { hatch } from 'ldrs'
 import { isMobile } from '../utils/screen'
-import { loginWithGoogle } from '../utils/google'
 hatch.register()
 
 
