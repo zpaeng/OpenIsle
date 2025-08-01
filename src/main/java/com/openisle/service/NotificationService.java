db31b5d6c151d31205753def45768fe646538c03
Tim <135014430+nagisa77@users.noreply.github.com>
2025-08-01 11:55:17 +0800

    Deduplicate post view notifications

--- src/main/java/com/openisle/service/NotificationService.java
+++ src/main/java/com/openisle/service/NotificationService.java
@@ -58,6 +58,9 @@ public class NotificationService {
 
     public Notification createNotification(User user, NotificationType type, Post post, Comment comment, Boolean approved,
                                            User fromUser, ReactionType reactionType, String content) {
+        if (type == NotificationType.POST_VIEWED && post != null && fromUser != null) {
+            notificationRepository.deleteByTypeAndFromUserAndPost(type, fromUser, post);
+        }
         Notification n = new Notification();
         n.setUser(user);
         n.setType(type);
