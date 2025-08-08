9c1cedd172634b0976b76f81481763a76f98a2c2
tim <nagisa12164@163.com>
2025-08-09 01:16:37 +0800

    feature: delete vue3 CSR

--- frontend/src/utils/clearVditorStorage.js
+++
@@ -1,7 +0,0 @@
-export function clearVditorStorage() {
-  Object.keys(localStorage).forEach(key => {
-    if (key.startsWith('vditoreditor-') || key === 'vditor') {
-      localStorage.removeItem(key)
-    }
-  })
-}
--- frontend/src/utils/level.js
+++
@@ -1,7 +0,0 @@
-export const LEVEL_EXP = [100, 200, 300, 600, 1200, 10000]
-
-export const prevLevelExp = level => {
-  if (level <= 0) return 0
-  if (level - 1 < LEVEL_EXP.length) return LEVEL_EXP[level - 1]
-  return LEVEL_EXP[LEVEL_EXP.length - 1]
-}
