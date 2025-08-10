6ab4879968b74a34220ad86eeb427776c19df3b2
tim <nagisa12164@163.com>
2025-08-10 12:46:20 +0800

    fix: 解决帖子评论问题

--- frontend_nuxt/pages/posts/[id]/index.vue
+++ frontend_nuxt/pages/posts/[id]/index.vue
@@ -80,7 +80,7 @@
       </div>
 
       <CommentEditor @submit="postComment" :loading="isWaitingPostingComment" :disabled="!loggedIn"
-        :show-login-overlay="!loggedIn" />
+        :show-login-overlay="!loggedIn" :parent-user-name="author.username" />
 
       <div class="comment-config-container">
         <div class="comment-sort-container">
@@ -390,7 +390,7 @@ export default {
       }
     }
 
-    const postComment = async (text, clear) => {
+    const postComment = async (parentUserName, text, clear) => {
       if (!text.trim()) return
       console.debug('Posting comment', { postId, text })
       isWaitingPostingComment.value = true
