bcdac9d9b25d2d897a2dee75c607f7dcbd10697f
tim <nagisa12164@163.com>
2025-08-14 17:27:30 +0800

    fix: delete hook update

--- frontend_nuxt/plugins/soft-manifest.client.ts
+++
@@ -1,9 +0,0 @@
-import { defineNuxtPlugin } from 'nuxt/app'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  // 覆盖默认行为：收到 manifest 更新时，不立刻在路由切换里刷新
-  nuxtApp.hooks.hook('app:manifest:update', () => {
-    // todo 选择：弹个提示，让用户点击刷新；或延迟到页面隐藏时再刷新
-    // 例如：document.addEventListener('visibilitychange', () => { if (document.hidden) location.reload() })
-  })
-})
