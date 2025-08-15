2e749a56729665f414becf6aca3c2c31528fae60
tim <nagisa12164@163.com>
2025-08-15 18:16:10 +0800

    fix: update 后端端口

--- backend/src/main/java/com/openisle/config/SecurityConfig.java
+++ backend/src/main/java/com/openisle/config/SecurityConfig.java
@@ -75,9 +75,11 @@ public class SecurityConfig {
         cfg.setAllowedOrigins(List.of(
                 "http://127.0.0.1:8080",
                 "http://127.0.0.1:3000",
+                "http://127.0.0.1:3001",
                 "http://127.0.0.1",
                 "http://localhost:8080",
                 "http://localhost:3000",
+                "http://localhost:3001",
                 "http://localhost",
                 "http://30.211.97.238:3000",
                 "http://30.211.97.238",
