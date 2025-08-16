1f74a29dceb6795cccaef1064f06592052add476
tim <nagisa12164@163.com>
2025-08-16 11:34:03 +0800

    fix: 修复header 显示异常

--- frontend_nuxt/components/HeaderComponent.vue
+++ frontend_nuxt/components/HeaderComponent.vue
@@ -184,8 +184,8 @@ onMounted(async () => {
 <style scoped>
 .header {
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  justify-content: center;
   height: var(--header-height);
   background-color: var(--background-color-blur);
   backdrop-filter: blur(10px);
